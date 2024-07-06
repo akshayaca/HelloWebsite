6ecaaae02af5f384e18ae91608872a373e98486e
akshayaca <akshayaca@outlook.com>
2024-07-05 20:19:52 -0400

    hello

---
+++ app.js
@@ -0,0 +1,28 @@
+// app.js
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as the template engine
+app.set('view engine', 'ejs');
+
+// Define a route to serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/employee', (req, res) => {
+    const currentDate = new Date().toLocaleString();
+    res.render('employee',{currentDate});
+   
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
